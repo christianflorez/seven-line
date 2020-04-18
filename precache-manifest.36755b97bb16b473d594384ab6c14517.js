6e31bc5d4290726b5db15c57ea8c63aefc30c4b6
Christian Florez <c-christian.florez@charter.com>
2020-04-17 19:09:00 -0600

    Updates

---
+++ precache-manifest.36755b97bb16b473d594384ab6c14517.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3d16a81f2a1c487ae162f043aab272",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8af93bd5b77661b7886a",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "373300fc52e6bbea3423",
+    "url": "/static/css/main.899d3a87.chunk.css"
+  },
+  {
+    "revision": "8af93bd5b77661b7886a",
+    "url": "/static/js/2.4edb792a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4edb792a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "373300fc52e6bbea3423",
+    "url": "/static/js/main.300b658e.chunk.js"
+  },
+  {
+    "revision": "a0bf3f14dd60d95572c9",
+    "url": "/static/js/runtime-main.b0d8ae98.js"
+  }
+]);
