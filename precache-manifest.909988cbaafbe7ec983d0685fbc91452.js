8b1c4c363355d6ab63bef891368e153d90fe488d
Christian Florez <florez.christian@gmail.com>
2024-04-30 10:11:50 -0600

    Updates

---
+++ precache-manifest.909988cbaafbe7ec983d0685fbc91452.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1af7c7ecda5702894e2d1372acf22f39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a067eeb71cf7e5d2ea2",
+    "url": "/static/css/2.06a9cb48.chunk.css"
+  },
+  {
+    "revision": "3cd76a609b44ed58178e",
+    "url": "/static/css/main.13bf7397.chunk.css"
+  },
+  {
+    "revision": "2a067eeb71cf7e5d2ea2",
+    "url": "/static/js/2.0f3756b6.chunk.js"
+  },
+  {
+    "revision": "60d722bf7039a99495b6931ad3067f54",
+    "url": "/static/js/2.0f3756b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cd76a609b44ed58178e",
+    "url": "/static/js/main.9d7b99d6.chunk.js"
+  },
+  {
+    "revision": "ba9575e119d9cf360d0f",
+    "url": "/static/js/runtime-main.789b17d6.js"
+  }
+]);
